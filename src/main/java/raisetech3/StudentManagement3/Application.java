d98fbba44a7e47c4e678cc213f133f1fa6721b29
Ai-cloud-rain <91471536+Ai-cloud-rain@users.noreply.github.com>
2024-10-23 16:33:02 +0900

    READ処理のServiceとController部分の実装（授業）

--- src/main/java/raisetech3/StudentManagement3/Application.java
+++ src/main/java/raisetech3/StudentManagement3/Application.java
@@ -1,34 +1,13 @@
-//課題２３
+//課題２4
 package raisetech3.StudentManagement3;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class Application {
 
-	@Autowired
-	private StudentRepository repository;
-
-	// メインメソッド（アプリケーションのエントリーポイント）
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	// ①学生情報のリストを取得するエンドポイント
-	@GetMapping("/studentList")
-	public List<Student> getStudentList() {
-		return repository.search();
-	}
-	// ②コース情報のリストを取得するエンドポイント
-	@GetMapping("/studentsCourseList")
-	public List<StudentsCourses> getStudentCourseList(){
-		return repository.searchStudentsCourses();
 	}
 }
--- src/main/java/raisetech3/StudentManagement3/data/StudentsCourses.java
+++ src/main/java/raisetech3/StudentManagement3/data/StudentsCourses.java
@@ -1,4 +1,4 @@
-package raisetech3.StudentManagement3;
+package raisetech3.StudentManagement3.data;
 
 import java.time.LocalDateTime;
 import lombok.Getter;
